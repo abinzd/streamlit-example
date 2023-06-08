a910eecbc02705cd4b922eee38a1180cef1b4f6b
Abin vinoy <134992947+abinzd@users.noreply.github.com>
2023-06-08 20:20:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,59 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
-import streamlit as st
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Random Quote Generator</title>
+  <style>
+    body {
+      font-family: Arial, sans-serif;
+      text-align: center;
+      margin-top: 50px;
+    }
 
-"""
-# Welcome to Streamlit!
+    h1 {
+      color: #333;
+    }
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+    .quote {
+      margin-top: 30px;
+      font-size: 20px;
+    }
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+    button {
+      margin-top: 20px;
+      padding: 10px 20px;
+      font-size: 16px;
+      background-color: #333;
+      color: #fff;
+      border: none;
+      cursor: pointer;
+    }
+  </style>
+</head>
+<body>
+  <h1>Random Quote Generator</h1>
+  <div class="quote">
+    <p id="quoteText">Click the button to generate a random quote.</p>
+  </div>
+  <button id="generateButton" onclick="generateQuote()">Generate Quote</button>
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+  <script>
+    // Array of quotes
+    var quotes = [
+      "The only way to do great work is to love what you do. - Steve Jobs",
+      "Innovation distinguishes between a leader and a follower. - Steve Jobs",
+      "Stay hungry, stay foolish. - Steve Jobs",
+      "Success is not final, failure is not fatal: It is the courage to continue that counts. - Winston Churchill",
+      "The best way to predict the future is to create it. - Peter Drucker",
+      "Believe you can and you're halfway there. - Theodore Roosevelt",
+      "The future belongs to those who believe in the beauty of their dreams. - Eleanor Roosevelt",
+      "The only limit to our realization of tomorrow will be our doubts of today. - Franklin D. Roosevelt"
+    ];
 
-
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+    // Function to generate a random quote
+    function generateQuote() {
+      var randomNumber = Math.floor(Math.random() * quotes.length);
+      document.getElementById("quoteText").innerHTML = quotes[randomNumber];
+    }
+  </script>
+</body>
+</html>
