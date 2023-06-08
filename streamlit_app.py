5a571444af2122820745c395d1c41fc5434d0aef
Abin vinoy <134992947+abinzd@users.noreply.github.com>
2023-06-08 20:26:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,59 +1,30 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Random Quote Generator</title>
-  <style>
-    body {
-      font-family: Arial, sans-serif;
-      text-align: center;
-      margin-top: 50px;
-    }
+import streamlit as st
+from pytube import YouTube
 
-    h1 {
-      color: #333;
-    }
+# Page setup
+st.title("YouTube Downloader")
 
-    .quote {
-      margin-top: 30px;
-      font-size: 20px;
-    }
+# Get YouTube video URL from user
+video_url = st.text_input("Enter YouTube video URL:", "")
 
-    button {
-      margin-top: 20px;
-      padding: 10px 20px;
-      font-size: 16px;
-      background-color: #333;
-      color: #fff;
-      border: none;
-      cursor: pointer;
-    }
-  </style>
-</head>
-<body>
-  <h1>Random Quote Generator</h1>
-  <div class="quote">
-    <p id="quoteText">Click the button to generate a random quote.</p>
-  </div>
-  <button id="generateButton" onclick="generateQuote()">Generate Quote</button>
+# Process the video URL
+if st.button("Download"):
+    if video_url:
+        try:
+            st.info("Downloading video... Please wait.")
+            
+            # Create a YouTube object
+            yt = YouTube(video_url)
 
-  <script>
-    // Array of quotes
-    var quotes = [
-      "The only way to do great work is to love what you do. - Steve Jobs",
-      "Innovation distinguishes between a leader and a follower. - Steve Jobs",
-      "Stay hungry, stay foolish. - Steve Jobs",
-      "Success is not final, failure is not fatal: It is the courage to continue that counts. - Winston Churchill",
-      "The best way to predict the future is to create it. - Peter Drucker",
-      "Believe you can and you're halfway there. - Theodore Roosevelt",
-      "The future belongs to those who believe in the beauty of their dreams. - Eleanor Roosevelt",
-      "The only limit to our realization of tomorrow will be our doubts of today. - Franklin D. Roosevelt"
-    ];
+            # Get the highest resolution stream
+            stream = yt.streams.get_highest_resolution()
 
-    // Function to generate a random quote
-    function generateQuote() {
-      var randomNumber = Math.floor(Math.random() * quotes.length);
-      document.getElementById("quoteText").innerHTML = quotes[randomNumber];
-    }
-  </script>
-</body>
-</html>
+            # Download the video
+            stream.download()
+
+            st.success("Video downloaded successfully!")
+        
+        except Exception as e:
+            st.error(f"An error occurred: {str(e)}")
+    else:
+        st.warning("Please enter a valid YouTube video URL.")
