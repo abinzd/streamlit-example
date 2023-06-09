e24fdd3aa6b911bfd0e659b0c901484d99fab7db
Abin vinoy <134992947+abinzd@users.noreply.github.com>
2023-06-09 20:04:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,44 +1,34 @@
 import streamlit as st
-import requests
-from bs4 import BeautifulSoup
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
-def get_product_price_amazon(product_url):
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
-    response = requests.get(product_url, headers=headers)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    price_element = soup.find(id='priceblock_ourprice')
-    if price_element is not None:
-        return price_element.get_text().strip()
-    else:
-        return 'Price not available'
+# Set up the YouTube Data API
+API_KEY = 'YOUR_YOUTUBE_API_KEY'
+YOUTUBE_CHANNEL_ID = 'YOUR_YOUTUBE_CHANNEL_ID'
+youtube = build('youtube', 'v3', developerKey=API_KEY)
 
-def get_product_price_flipkart(product_url):
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
-    response = requests.get(product_url, headers=headers)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    price_element = soup.find(class_='_1vC4OE_2rQ-NK')
-    if price_element is not None:
-        return price_element.get_text().strip()
-    else:
-        return 'Price not available'
+# Function to get the subscriber count
+def get_subscriber_count():
+    try:
+        response = youtube.channels().list(
+            part='statistics',
+            id=YOUTUBE_CHANNEL_ID
+        ).execute()
 
-def main():
-    st.title("Price Checker")
-    st.write("Welcome to the Price Checker!")
-    product_url = st.text_input("Enter the URL of the product:")
-    website_choice = st.selectbox("Select the website:", ['Amazon', 'Flipkart'])
+        items = response['items']
+        subscriber_count = int(items[0]['statistics']['subscriberCount'])
+        return subscriber_count
+    except HttpError as e:
+        st.error(f"An error occurred: {e}")
+        return None
 
-    if st.button("Check Price"):
-        if website_choice == 'Amazon':
-            product_price = get_product_price_amazon(product_url)
-            st.write('Amazon Price:', product_price)
-        elif website_choice == 'Flipkart':
-            product_price = get_product_price_flipkart(product_url)
-            st.write('Flipkart Price:', product_price)
-        else:
-            st.write('Invalid choice. Please try again.')
+# Streamlit app
+st.title('Real-time YouTube Subscribers')
+subscriber_count = get_subscriber_count()
 
+if subscriber_count is not None:
+    st.write(f"Subscriber count: {subscriber_count}")
+
+# Run the Streamlit app
 if __name__ == '__main__':
-    main()
+    st.run_app()
