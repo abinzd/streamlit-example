56e104d5a18682a7bb148b7c5f69d4c8de9f5cef
Abin vinoy <134992947+abinzd@users.noreply.github.com>
2023-06-09 20:26:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,26 @@
+import re
 import streamlit as st
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
 # Set up the YouTube Data API
 API_KEY = 'AIzaSyBIPX1w8_nZA-TIj6s1cFhwuOkTVADW0Bo'
-YOUTUBE_CHANNEL_ID = 'Vandipranthan'
 youtube = build('youtube', 'v3', developerKey=API_KEY)
 
+# Function to extract channel ID from YouTube link
+def extract_channel_id(link):
+    pattern = r"(?<=channel\/|user\/|u\/|embed\/|v=|watch\?v=|&v=|youtu.be\/|\/v\/|\/e\/|watch\?v%3D|watch\?feature=player_embedded&v=|%2Fvideos%2F|embed%\?video_id=|youtube.com\/user\/[^#]*#([^\/]*\/)*)[^&#\?\/\n]*"
+    match = re.search(pattern, link)
+    if match:
+        return match.group()
+    return None
+
 # Function to get the subscriber count
-def get_subscriber_count():
+def get_subscriber_count(channel_id):
     try:
         response = youtube.channels().list(
             part='statistics',
-            id=YOUTUBE_CHANNEL_ID
+            id=channel_id
         ).execute()
 
         items = response.get('items', [])
@@ -30,7 +38,16 @@ def get_subscriber_count():
 
 # Streamlit app
 st.title('Real-time YouTube Subscribers')
-subscriber_count = get_subscriber_count()
 
-if subscriber_count is not None:
-    st.write(f"Subscriber count: {subscriber_count}")
+# Accept YouTube link from user
+youtube_link = st.text_input('Enter a YouTube channel link:')
+channel_id = extract_channel_id(youtube_link)
+
+if channel_id:
+    subscriber_count = get_subscriber_count(channel_id)
+
+    if subscriber_count is not None:
+        st.write(f"Subscriber count: {subscriber_count}")
+else:
+    st.warning('Invalid YouTube channel link entered.')
+
